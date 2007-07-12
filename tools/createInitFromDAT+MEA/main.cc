f500d03a725bbe6fb0c180a2736bcc015e3c4a61
Patrik Sahlin <patrik.sahlin@thep.lu.se>
2007-07-12 08:41:11 +0000

    createInitFromDAT+MEA: Changed to old orientation

--- tools/createInitFromDAT+MEA/main.cc
+++ tools/createInitFromDAT+MEA/main.cc
@@ -520,8 +520,8 @@ void readMEAFile(std::istream &input)
 				dy /= A;
 				
 				direction->setIndex(n);
-				direction->setX(dx);
-				direction->setY(dy);
+				direction->setX(-dy);
+				direction->setY(dx);
 				direction->setCell(cell);
 				cell->setDirection(direction);
 
