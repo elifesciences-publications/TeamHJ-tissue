ce06c924121a88057ac1d934eb28cd60684da0b8
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2011-02-22 15:32:39 +0000

    Added file for triangular springs.

---
+++ src/mechanicalTRBS.cc
@@ -0,0 +1,104 @@
+//
+// Filename     : mechanicalTRBS.cc
+// Description  : Classes describing updates due to mechanical triangular biquadratic springs
+// Author(s)    : Henrik Jonsson (henrik@thep.lu.se)
+// Created      : February 2011
+// Revision     : $Id:$
+//
+#include <utility>
+#include <vector>
+#include "baseReaction.h"
+#include "mechanicalTRBS.h"
+#include "tissue.h"
+
+VertexFromTRBS::
+VertexFromTRBS(std::vector<double> &paraValue, 
+	       std::vector< std::vector<size_t> > 
+	       &indValue ) 
+{  
+  // Do some checks on the parameters and variable indeces
+  if( paraValue.size()!=2 ) {
+    std::cerr << "VertexFromTRBS::"
+	      << "VertexFromTRBS() "
+	      << "Uses two parameters young modulus and poisson coefficient.\n";
+    exit(0);
+  }
+  if( indValue.size()!=1 || indValue[0].size()!=1 ) { 
+    std::cerr << "VertexFromTRBS::"
+	      << "VertexFromTRBS() "
+	      << "Only wall length index given in first level." << std::endl;
+    exit(0);
+  }
+  
+  // Set the variable values
+  setId("VertexFromTRBS");
+  setParameter(paraValue);  
+  setVariableIndex(indValue);
+  
+  // Set the parameter identities
+  std::vector<std::string> tmp( numParameter() );
+  tmp[0] = "Y_mod";
+  tmp[1] = "P_ratio";
+  setParameterId( tmp );
+}
+
+void VertexFromTRBS::
+derivs(Tissue &T,
+       std::vector< std::vector<double> > &cellData,
+       std::vector< std::vector<double> > &wallData,
+       std::vector< std::vector<double> > &vertexData,
+       std::vector< std::vector<double> > &cellDerivs,
+       std::vector< std::vector<double> > &wallDerivs,
+       std::vector< std::vector<double> > &vertexDerivs ) {
+  
+  //Do the update for each cell
+  size_t numCells = T.numCell();
+  size_t wallLengthIndex = variableIndex(0,0);
+  
+  for( size_t i=0 ; i<numCells ; ++i ) {
+    if( T.cell(i).numWall() != 3 ) {
+      std::cerr << "VertexFromTRBS::derivs() only defined for triangular cells."
+		<< std::endl;
+      exit(-1);
+    }
+
+    size_t v1 = T.cell(i).vertex(0)->index();
+    size_t v2 = T.cell(i).vertex(1)->index();
+    size_t v3 = T.cell(i).vertex(2)->index();
+    size_t w1 = T.cell(i).wall(0)->index();
+    size_t w2 = T.cell(i).wall(1)->index();
+    size_t w3 = T.cell(i).wall(2)->index();
+    double L1 = wallData[w1][wallLengthIndex];
+    double L2 = wallData[w2][wallLengthIndex];
+    double L3 = wallData[w3][wallLengthIndex];
+
+
+    size_t dimension = vertexData[v1].size();
+    assert( vertexData[v2].size()==dimension );
+    //Calculate shared factors
+    double distance=0.0;
+    for( size_t d=0 ; d<dimension ; d++ )
+      distance += (vertexData[v1][d]-vertexData[v2][d])*
+	(vertexData[v1][d]-vertexData[v2][d]);
+    distance = std::sqrt(distance);
+    double wallLength=wallData[i][wallLengthIndex];
+    double coeff = parameter(0)*((1.0/wallLength)-(1.0/distance));
+    if( distance <= 0.0 && wallLength <=0.0 ) {
+      //std::cerr << i << " - " << wallLength << " " << distance << std::endl;
+      coeff = 0.0;
+    }
+    if( distance>wallLength )
+      coeff *=parameter(1);
+    
+    //Save force in wall variable if appropriate
+    if( numVariableIndexLevel()>1 )
+      wallData[i][variableIndex(1,0)] = coeff*distance;
+    
+    //Update both vertices for each dimension
+    for(size_t d=0 ; d<dimension ; d++ ) {
+      double div = (vertexData[v1][d]-vertexData[v2][d])*coeff;
+      vertexDerivs[v1][d] -= div;
+      vertexDerivs[v2][d] += div;
+    }
+  }
+}
