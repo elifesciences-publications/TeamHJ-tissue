eb65a756b945dcdb74ad7c547e7acffdce233194
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2011-02-28 10:06:18 +0000

    updated mechanicalTRBS.cc

--- src/mechanicalTRBS.cc
+++ src/mechanicalTRBS.cc
@@ -54,9 +54,10 @@ derivs(Tissue &T,
   //Do the update for each cell
   size_t numCells = T.numCell();
   size_t wallLengthIndex = variableIndex(0,0);
-  
+  size_t numWalls = 3; // defined only for triangles at the moment
+
   for( size_t i=0 ; i<numCells ; ++i ) {
-    if( T.cell(i).numWall() != 3 ) {
+    if( T.cell(i).numWall() != numWalls ) {
       std::cerr << "VertexFromTRBS::derivs() only defined for triangular cells."
 		<< std::endl;
       exit(-1);
@@ -68,13 +69,25 @@ derivs(Tissue &T,
     size_t w1 = T.cell(i).wall(0)->index();
     size_t w2 = T.cell(i).wall(1)->index();
     size_t w3 = T.cell(i).wall(2)->index();
-    double L1 = wallData[w1][wallLengthIndex];
-    double L2 = wallData[w2][wallLengthIndex];
-    double L3 = wallData[w3][wallLengthIndex];
-
+    std::vector<double> restingLength(numWalls);
+    restingLength[0] = wallData[w1][wallLengthIndex];
+    restingLength[1] = wallData[w2][wallLengthIndex];
+    restingLength[2] = wallData[w3][wallLengthIndex];
+    std::vector< std::vector<double> > position(3,vertexData[v1]);
+    position[1] = vertexData[v2];
+    position[2] = vertexData[v3];
+    //position[0][0] x for vertex 1 (of the cell)
+    //position[1][1] y for vertex 2 (of the cell)
+    //position[2][2] z for vertex 3 (of the cell)
+    std::vector<double> length(numWalls);
+    length[0] = T.wall(w1)->lengthFromVertexPosition(vertexData);
+    length[1] = T.wall(w2)->lengthFromVertexPosition(vertexData);
+    length[2] = T.wall(w3)->lengthFromVertexPosition(vertexData);
 
     size_t dimension = vertexData[v1].size();
     assert( vertexData[v2].size()==dimension );
+    assert( vertexData[v3].size()==dimension );
+
     //Calculate shared factors
     double distance=0.0;
     for( size_t d=0 ; d<dimension ; d++ )
