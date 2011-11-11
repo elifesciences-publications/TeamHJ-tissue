fc15f953eca99c06879df4916327ed61050e6f9f
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2011-11-11 21:52:32 +0000

    Added myTypedefs.h to store DataMatrix.

---
+++ src/myTypedefs.h
@@ -0,0 +1,11 @@
+#ifndef MYTYPEDEFS_H
+#define MYTYPEDEFS_H
+//
+// Filename     : myTypedefs.h
+// Description  : Contains all typedefs used within the tissue code
+// Author(s)    : Henrik Jonsson (henrik@thep.lu.se)
+// Created      : November 2011
+//
+typedef std::vector< std::vector<double> > DataMatrix;
+
+#endif
