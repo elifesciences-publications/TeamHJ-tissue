b251d36589e7d02deff2759952b8a1c0eaacd543
Behruz Bozorg <behruz@thep.lu.se>
2012-04-10 08:44:41 +0000

    adding the function VertexFromTRBSCenterTriangulationMT together with strain stress calculations

--- src/mechanicalTRBS.h
+++ src/mechanicalTRBS.h
@@ -261,4 +261,87 @@ class VertexFromTRBSMT : public BaseReaction {
 };
 
 
+
+///
+/// @brief Triangular spring model for plates (2D walls) assuming
+/// triangulation with a central point on the 2D wall/cell.
+///
+/// The update (in all dimensions) are given by
+///
+/// @f[ \frac{dx_i}{dt} = ... @f]
+///
+/// ...
+///
+/// The theory of the mechanical model comes from H. Delingette,
+/// Triangular springs for modelling non-linear membranes, IEEE Trans
+/// Vis Comput Graph 14, 329-41 (2008)
+///
+/// In a model file the reaction is defined as
+///
+/// @verbatim
+/// VertexFromTRBScenterTriangulationMT 4 2 2 1
+/// Y_modulus_Longitudinal P_coeff_Longitudinal Y_modulus_Transverse P_coeff_Transverse 
+/// L_ij-index MT_cellIndex
+/// InternalVarStartIndex
+
+/// VertexFromTRBScenterTriangulation 2 2 1 1
+/// Y_modulus P_coeff
+/// L_ij-index
+/// InternalVarStartIndex
+
+/// VertexFromTRBS 2 1 1
+/// Y_modulus P_coeff
+/// L_ij-index
+
+
+/// VertexFromTRBSMT 4 1 2
+/// Y_modulus_Longitudinal P_coeff_Longitudinal Y_modulus_Transverse P_coeff_Transverse 
+/// L_ij-index MT_cellIndex
+/// @endverbatim
+///
+class VertexFromTRBScenterTriangulationMT : public BaseReaction {
+  
+ public:
+  ///
+  /// @brief Main constructor
+  ///
+  /// This is the main constructor which sets the parameters and variable
+  /// indices that defines the reaction.
+  ///
+  /// @param paraValue vector with parameters
+  ///
+  /// @param indValue vector of vectors with variable indices
+  ///
+  /// @see BaseReaction::createReaction(std::vector<double> &paraValue,...)
+  ///
+  VertexFromTRBScenterTriangulationMT(std::vector<double> &paraValue, 
+				    std::vector< std::vector<size_t> > 
+				    &indValue );  
+  ///
+  /// @brief Derivative function for this reaction class
+  ///
+  /// @see BaseReaction::derivs(Tissue &T,...)
+  ///
+  void derivs(Tissue &T,
+	      DataMatrix &cellData,
+	      DataMatrix &wallData,
+	      DataMatrix &vertexData,
+	      DataMatrix &cellDerivs,
+	      DataMatrix &wallDerivs,
+	      DataMatrix &vertexDerivs );
+
+  ///
+  /// @brief Reaction initiation applied before simulation starts
+  ///
+  /// @see BaseReaction::initiate(Tissue &T,...)
+  ///
+  void initiate(Tissue &T,
+		DataMatrix &cellData,
+		DataMatrix &wallData,
+		DataMatrix &vertexData,
+		DataMatrix &cellDerivs,
+		DataMatrix &wallDerivs,
+		DataMatrix &vertexDerivs );  
+};
+
 #endif
