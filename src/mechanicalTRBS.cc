ceb0c974828de42572830922063808ca524620d4
Behruz Bozorg <behruz@thep.lu.se>
2012-11-01 19:47:17 +0000

    removing initiate from VertexFromTRBScenterTriangulationMT

--- src/mechanicalTRBS.cc
+++ src/mechanicalTRBS.cc
@@ -5522,52 +5522,52 @@ derivs(Tissue &T,
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-void VertexFromTRBScenterTriangulationMT::
-initiate(Tissue &T,
-	 DataMatrix &cellData,
-	 DataMatrix &wallData,
-	 DataMatrix &vertexData,
-	 DataMatrix &cellDerivs,
-	 DataMatrix &wallDerivs,
-	 DataMatrix &vertexDerivs)
-{
-  size_t dimension=3; //Only implemented for 3D models
-  assert (dimension==vertexData[0].size());
-  size_t numVariable = T.cell(0).numVariable();
-  assert (numVariable==cellData[0].size());
-  // Create the new variables
-  if (variableIndex(1,0) != numVariable) {
-    std::cerr << "VertexFromTRBScenterTriangulation::initiate() "
-	      << "Wrong index given as start index for additional variables."
-	      << std::endl;
-    exit(-1);
-  }
-  size_t numCell = cellData.size();
-  assert (numCell==T.numCell());
-  std::vector<double> com(dimension);
+// void VertexFromTRBScenterTriangulationMT::
+// initiate(Tissue &T,
+// 	 DataMatrix &cellData,
+// 	 DataMatrix &wallData,
+// 	 DataMatrix &vertexData,
+// 	 DataMatrix &cellDerivs,
+// 	 DataMatrix &wallDerivs,
+// 	 DataMatrix &vertexDerivs)
+// {
+//   size_t dimension=3; //Only implemented for 3D models
+//   assert (dimension==vertexData[0].size());
+//   size_t numVariable = T.cell(0).numVariable();
+//   assert (numVariable==cellData[0].size());
+//   // Create the new variables
+//   if (variableIndex(1,0) != numVariable) {
+//     std::cerr << "VertexFromTRBScenterTriangulation::initiate() "
+// 	      << "Wrong index given as start index for additional variables."
+// 	      << std::endl;
+//     exit(-1);
+//   }
+//   size_t numCell = cellData.size();
+//   assert (numCell==T.numCell());
+//   std::vector<double> com(dimension);
   
-  for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {
-    size_t numInternalWall = T.cell(cellIndex).numVertex();
-    cellData[cellIndex].resize(numVariable+dimension+numInternalWall);
-    cellDerivs[cellIndex].resize(numVariable+dimension+numInternalWall);
-    com = T.cell(cellIndex).positionFromVertex(vertexData);
-    // Set center position to com of the cell
-    for (size_t d=0; d<dimension; ++d)
-      cellData[cellIndex][numVariable+d] = com[d];    
-    // Set internal wall lengths to the distance btw com and the vertex
-    for (size_t wallindex=0; wallindex<numInternalWall; ++wallindex) {
-      Vertex *tmpVertex = T.cell(cellIndex).vertex(wallindex); 
-      size_t vertexIndex = tmpVertex->index();
-      double distance = std::sqrt( (com[0]-vertexData[vertexIndex][0])*
-				   (com[0]-vertexData[vertexIndex][0])+
-				   (com[1]-vertexData[vertexIndex][1])*
-				   (com[1]-vertexData[vertexIndex][1])+
-				   (com[2]-vertexData[vertexIndex][2])*
-				   (com[2]-vertexData[vertexIndex][2]) );   
-      cellData[cellIndex][numVariable+dimension+wallindex] = distance;
-    }
-  }
-}
+//   for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {
+//     size_t numInternalWall = T.cell(cellIndex).numVertex();
+//     cellData[cellIndex].resize(numVariable+dimension+numInternalWall);
+//     cellDerivs[cellIndex].resize(numVariable+dimension+numInternalWall);
+//     com = T.cell(cellIndex).positionFromVertex(vertexData);
+//     // Set center position to com of the cell
+//     for (size_t d=0; d<dimension; ++d)
+//       cellData[cellIndex][numVariable+d] = com[d];    
+//     // Set internal wall lengths to the distance btw com and the vertex
+//     for (size_t wallindex=0; wallindex<numInternalWall; ++wallindex) {
+//       Vertex *tmpVertex = T.cell(cellIndex).vertex(wallindex); 
+//       size_t vertexIndex = tmpVertex->index();
+//       double distance = std::sqrt( (com[0]-vertexData[vertexIndex][0])*
+// 				   (com[0]-vertexData[vertexIndex][0])+
+// 				   (com[1]-vertexData[vertexIndex][1])*
+// 				   (com[1]-vertexData[vertexIndex][1])+
+// 				   (com[2]-vertexData[vertexIndex][2])*
+// 				   (com[2]-vertexData[vertexIndex][2]) );   
+//       cellData[cellIndex][numVariable+dimension+wallindex] = distance;
+//     }
+//   }
+//}
 
 
 
