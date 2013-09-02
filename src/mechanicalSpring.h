bc7ab58f4529f2d1dd1ab05f5b24fe6fa3712193
Behruz Bozorg <behruz@thep.lu.se>
2013-09-02 15:34:24 +0000

    VertexFromExternalSpringsFromPerpVertex

--- src/mechanicalSpring.h
+++ src/mechanicalSpring.h
@@ -682,10 +682,11 @@ class VertexFromExternalSpring : public BaseReaction {
 ///
 /// @verbatim
 ///
-/// VertexFromExternalSpringFromPerpVertex 5 1 2 
-/// K   f_adh Lmaxfactor growth_rate  intraction_angle
+/// VertexFromExternalSpringFromPerpVertex 6 1 3 
+/// K   f_adh Lmaxfactor growth_rate  intraction_angle corner_angle
 /// growth_flag (0:non ,1: ,2: ,3: ,4: ,5: ,6: )
 /// connection_flag (1: constraint on the first node only, 2: for both)
+/// exclude_corner_flag (0:include corners, 1: exclude corners)
 /// @endverbatim
 ///
 ///
