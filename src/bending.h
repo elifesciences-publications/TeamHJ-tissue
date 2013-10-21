d187e22a106957b286d73aca99944435ac1677c7
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2013-10-21 17:57:39 +0000

    Started the addition of the bending moment with a preferred angle that can also be relaxed towards the current angle.

--- src/bending.h
+++ src/bending.h
@@ -78,5 +78,143 @@ namespace Bending {
 		DataMatrix &wallDerivs,
 		DataMatrix &vertexDerivs );
   };
+
+  /// 
+  /// @brief Creates a bending resistance by forces to keep an angle at a specific value
+  ///
+  /// Each vertex has a prefered angle, and a bendng moment is introduced to
+  /// move vertices to keep this angle. The update for the angle at a specific vertex comes from the potential
+  /// 
+  /// @f[ V(\Theta,\Theta^{t}) = \frac{1}{2}k_{\Theta} (\Theta-\Theta^{t})^2 @f]
+  ///
+  /// where vertex positions are updated according to
+  ///
+  /// @f[ \frac{dx_i}{dt} = -\frac{dV}{dx} = \frac{dV}{d\Theta}\frac{d\Theta}{dF}\frac{dF}{dx_i}  @f]
+  ///
+  /// where the angle is calculated by
+  ///
+  /// @f[ \Theta = acos(F) \pm \Pi @f]
+  ///
+  /// with
+  ///
+  /// @f[ F = \frac{x_1 \dot x_2}{|x_1||x_2|} = \frac{f}{g} @f]
+  ///
+  /// where x1, x2, are the two edges joined at the vertex, -/+ comes from if the structure is convex/concave. 
+  /// For each angle three edges are involved and contribute (in all dimensions) to the update (assuming x-,x,x+
+  /// are positions for the vertices:
+  ///
+  /// @f[ \frac{dx_{-}}{dt} = k_{\Theta} (\Theta-\Theta^{t}) \frac{1}{(1 - F^2)^{0.5}} 
+  /// \frac{1}{g} [(x-x_+)+(x-x_-) \frac{f x_2}{g x_1}] @f]
+  /// @f[ \frac{dx}{dt} = k_{\Theta} (\Theta-\Theta^{t}) \frac{1}{(1 - F^2)^{0.5}}
+  /// \frac{1}{g} [(x_+ - x)(1+\frac{f x_1}{g x_2}) + (x_- - x) (1+\frac{f x_2}{g x_1})] @f]
+  /// @f[ \frac{dx_{+}}{dt} = k_{\Theta} (\Theta-\Theta^{t}) \frac{1}{(1 - F^2)^{0.5}}
+  /// \frac{1}{g} [(x-x_-)+(x-x_+) \frac{f x_1}{g x_2}] @f]
+  ///
+  /// The reaction expects a strength of the bending force, and the edge variable index for the length (=0). 
+  /// The prefered angle is stored as a second wall variable. In a model file it is defined as:
+  /// @verbatim
+  /// Bending::Angle 1 1 2
+  /// k_theta
+  /// L_index Theta^t_index
+  /// @endverbatim
+  ///
+  /// @See Bending::AngleRelax for how to update the prefered angle towards the current.
+  /// @See Bending::AngleInitiate will initiate the stored angle to the current measured angle.
+  ///
+  class Angle : public BaseReaction {
+    
+  public:
+    ///
+    /// @brief Main constructor
+    ///
+    /// This is the main constructor which sets the parameters and variable
+    /// indices that defines the reaction.
+    ///
+    /// @param paraValue vector with parameters
+    ///
+    /// @param indValue vector of vectors with variable indices
+    ///
+    /// @see BaseReaction::createReaction(std::vector<double> &paraValue,...)
+    ///
+    Angle(std::vector<double> &paraValue, 
+	  std::vector< std::vector<size_t> > 
+	  &indValue );
+    
+    ///
+    /// @brief Derivative function for this reaction class
+    ///
+    /// For this reaction nothing is added to the derivative
+    ///
+    /// @see BaseReaction::derivs(Compartment &compartment,size_t species,...)
+    ///
+    void derivs(Tissue &T,
+		DataMatrix &cellData,
+		DataMatrix &wallData,
+		DataMatrix &vertexData,
+		DataMatrix &cellDerivs,
+		DataMatrix &wallDerivs,
+		DataMatrix &vertexDerivs );
+  };
+
+  /// 
+  /// @brief Initiates an angle variable to the current value of the angle at vertices
+  ///
+  /// Sets the prefered angle for a vertex to the initial calculated value.
+  /// The angle is calculated by
+  ///
+  /// @f[ \Theta = acos(F) \pm \Pi @f]
+  ///
+  /// with
+  ///
+  /// @f[ F = \frac{x_1 \dot x_2}{|x_1||x_2|} = \frac{f}{g} @f]
+  ///
+  /// where x1, x2, are the two edges joined at the vertex, -/+ comes from if the structure is convex/concave. 
+  ///
+  /// @verbatim
+  /// Bending::AngleInitiate 0 1 2
+  /// L_index Theta^t_index
+  /// @endverbatim
+  ///
+  /// where the first index is the wall length index (=0) and the second is where the angle is stored.
+  ///
+  /// @See Bending::Angle for how to update the vertices towards this angle 
+  /// @See Bending::AngleRelax for how to update the prefered angle towards the current.
+  ///
+  class AngleInitiate : public BaseReaction {
+    
+  public:
+    ///
+    /// @brief Main constructor
+    ///
+    /// This is the main constructor which sets the parameters and variable
+    /// indices that defines the reaction.
+    ///
+    /// @param paraValue vector with parameters
+    ///
+    /// @param indValue vector of vectors with variable indices
+    ///
+    /// @see BaseReaction::createReaction(std::vector<double> &paraValue,...)
+    ///
+    AngleInitiate(std::vector<double> &paraValue, 
+		  std::vector< std::vector<size_t> > 
+		  &indValue );
+    
+    ///
+    /// @brief Derivative function for this reaction class
+    ///
+    /// For this reaction nothing is added to the derivative
+    ///
+    /// @see BaseReaction::derivs(Compartment &compartment,size_t species,...)
+    ///
+    void derivs(Tissue &T,
+		DataMatrix &cellData,
+		DataMatrix &wallData,
+		DataMatrix &vertexData,
+		DataMatrix &cellDerivs,
+		DataMatrix &wallDerivs,
+		DataMatrix &vertexDerivs );
+  };
+
+  
 }
 #endif
