5f5f00bb279e1d725cc4b9296e78584ca5747b49
Behruz Bozorg <behruz@thep.lu.se>
2014-08-08 10:53:12 +0000

    reaction Branching added

--- src/compartmentDivision.h
+++ src/compartmentDivision.h
@@ -56,6 +56,48 @@ class DivisionVolumeViaLongestWall : public BaseCompartmentChange {
 };
 
 ///
+/// @brief Divides a cell when volume above a threshold, with new wall perpendicular to the longest wall segment.
+///
+/// Creates a branch as
+/// @verbatim
+/// Branching 3 1 [1]
+/// V_th L^{wall}_{frac} L^{wall}_{threshold}
+/// I1
+/// @endverbatim
+///
+/// where @f$V_{th}@f$ is the cell volume threshold, @f$L^{wall}_{frac}@f$ is the resting length 
+/// of the new wall (1.0 sets it to the distance between the vertices, and @f$L^{wall}_{threshold}@f$ 
+/// is the smallest (relative) length of the new subwalls (i.e. if closer than this to an existing vertex
+/// it will be moved to this distance from the old vertex).
+///
+/// The list of indices given are for those variables that need to be updated due to the division,
+/// e.g. concentrations do not, the volume itself (if stored) needs to as well as molecular numbers.
+/// 
+class Branching : public BaseCompartmentChange {
+  
+ public:
+  
+  Branching(std::vector<double> &paraValue, 
+			       std::vector< std::vector<size_t> > 
+			       &indValue );
+  int flag(Tissue *T,size_t i,
+	   DataMatrix &cellData,
+	   DataMatrix &wallData,
+	   DataMatrix &vertexData,
+	   DataMatrix &cellDerivs,
+	   DataMatrix &wallDerivs,
+	   DataMatrix &vertexDerivs );
+  void update(Tissue* T,size_t i,
+	      DataMatrix &cellData,
+	      DataMatrix &wallData,
+	      DataMatrix &vertexData,
+	      DataMatrix &cellDerivs,
+	      DataMatrix &wallDerivs,
+	      DataMatrix &vertexDerivs );  
+};
+
+
+///
 /// @brief Divides a cell when volume above a threshold and cell close enough to 'apex'
 ///
 /// Divides a cell when volume above a threshold and cell within a distance
