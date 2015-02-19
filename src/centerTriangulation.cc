f34c18eb1f5a42d5a514b13f1fc0023cc9e9eb15
Behruz Bozorg <behruz@thep.lu.se>
2015-02-19 16:42:47 +0000

    initiate updated

--- src/centerTriangulation.cc
+++ src/centerTriangulation.cc
@@ -20,10 +20,13 @@ namespace CenterTriangulation {
 	   std::vector< std::vector<size_t> > &indValue)
   {
     // Do some checks on the parameters and variable indeces
-    if( paraValue.size()!=0 && paraValue.size()!=1 ) {
+    if( paraValue.size()!=0 && paraValue.size()!=1 && paraValue.size()!=2 ) {
       std::cerr << "CenterTriangulation::Initiate::Initiate() "
-		<< "Uses zero or one parameter. If one provided it is a flag equal to one for overriding "
-		<< "a central triangulation stored in tissue (generated from scratch instead)." << std::endl;
+		<< "Uses zero or one or two parameter(s). If one provided it is a flag equal to one for overriding "
+                << "If two parameters provided the second should be equal to one for initiation for two "
+                << " independent resting  lengths for neigbohr elements used for growth dependent reactions. "
+		<< "a central triangulation stored in tissue (generated from scratch instead)." 
+                << std::endl;
       exit(EXIT_FAILURE);
     }
     if( indValue.size()!=1 || indValue[0].size()!=1 ) { 
@@ -41,6 +44,9 @@ namespace CenterTriangulation {
     std::vector<std::string> tmp( numParameter() );
     if (paraValue.size())
       tmp[0] = "overRideFlag";
+    if (paraValue.size()==2)
+      tmp[1] = "doubleRestingLength";
+  
     setParameterId( tmp );    
   }
   
@@ -56,6 +62,7 @@ namespace CenterTriangulation {
     size_t dimension=3; //Only implemented for 3D models
     assert (dimension==vertexData[0].size());
     size_t numVariable = T.cell(0).numVariable();
+    std::cerr<<" here..............."<<numVariable<<" "<<cellData[0].size()<<std::endl;
     assert (numVariable==cellData[0].size());
 
     // Create the new variables
@@ -68,55 +75,143 @@ namespace CenterTriangulation {
     size_t numCell = cellData.size();
     std::vector<double> com(dimension);    
     assert (numCell==T.numCell());
-    if (!T.cell(0).numCenterPosition() || (numParameter() && parameter(0)==1) ) {
-      //No centers defined in cells in tissue or overridden (create central triangulation from scratch)
-      for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {	
-        size_t numInternalWall = T.cell(cellIndex).numVertex();
-	cellData[cellIndex].resize(numVariable+dimension+numInternalWall);
-	cellDerivs[cellIndex].resize(numVariable+dimension+numInternalWall);
-	com = T.cell(cellIndex).positionFromVertex(vertexData);
-	// Set center position to com of the cell
-	for (size_t d=0; d<dimension; ++d)
-	  cellData[cellIndex][numVariable+d] = com[d];    
-	// Set internal wall lengths to the distance btw com and the vertex
-	for (size_t k=0; k<numInternalWall; ++k) {
-	  Vertex *tmpVertex = T.cell(cellIndex).vertex(k); 
-	  size_t vertexIndex = tmpVertex->index();
-	  double distance = std::sqrt( (com[0]-vertexData[vertexIndex][0])*
-				       (com[0]-vertexData[vertexIndex][0])+
-				       (com[1]-vertexData[vertexIndex][1])*
-				       (com[1]-vertexData[vertexIndex][1])+
-				       (com[2]-vertexData[vertexIndex][2])*
-				       (com[2]-vertexData[vertexIndex][2]) );   
-	  cellData[cellIndex][numVariable+dimension+k] = distance;
-	}
+    if (numParameter()==0 || numParameter()==1){
+      if (!T.cell(0).numCenterPosition() || (numParameter() && parameter(0)==1) ) {
+        
+        //No centers defined in cells in tissue or overridden (create central triangulation from scratch)
+        for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {	
+          size_t numInternalWall = T.cell(cellIndex).numVertex();
+          cellData[cellIndex].resize(numVariable+dimension+numInternalWall);
+          cellDerivs[cellIndex].resize(numVariable+dimension+numInternalWall);
+          com = T.cell(cellIndex).positionFromVertex(vertexData);
+          // Set center position to com of the cell
+          for (size_t d=0; d<dimension; ++d)
+            cellData[cellIndex][numVariable+d] = com[d];    
+          // Set internal wall lengths to the distance btw com and the vertex
+          for (size_t k=0; k<numInternalWall; ++k) {
+            Vertex *tmpVertex = T.cell(cellIndex).vertex(k); 
+            size_t vertexIndex = tmpVertex->index();
+            double distance = std::sqrt( (com[0]-vertexData[vertexIndex][0])*
+                                         (com[0]-vertexData[vertexIndex][0])+
+                                         (com[1]-vertexData[vertexIndex][1])*
+                                         (com[1]-vertexData[vertexIndex][1])+
+                                         (com[2]-vertexData[vertexIndex][2])*
+                                         (com[2]-vertexData[vertexIndex][2]) );   
+            cellData[cellIndex][numVariable+dimension+k] = distance;
+            
+          }
+          // sleep(1); //initiating MT dirrections randomly in xy plane
+          // srand((unsigned)time(NULL));
+        
+          // cellData[cellIndex][0]=((double)rand() / (RAND_MAX+1)) ;
+          // cellData[cellIndex][1]=((double)rand() / (RAND_MAX+1)) ;
+          // double tmp=-std::sqrt(cellData[cellIndex][0]*cellData[cellIndex][0]
+          //                      +cellData[cellIndex][1]*cellData[cellIndex][1]); 
+          
+          // cellData[cellIndex][0]/=tmp;
+          // cellData[cellIndex][1]/=tmp;
+          // std::cerr<< "MT vector"<< cellData[cellIndex][0]<<" "<< cellData[cellIndex][1]<< std::endl;
+        }
+      }
+      else { 
+        
+        // Copy central position and edge length data from cells in Tissue
+        for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {
+          size_t numInternalWall = T.cell(cellIndex).numVertex();
+          cellData[cellIndex].resize(numVariable+dimension+numInternalWall);
+          cellDerivs[cellIndex].resize(numVariable+dimension+numInternalWall);
+          com = T.cell(cellIndex).centerPosition();
 
-        // sleep(1); //initiating MT dirrections randomly in xy plane
-        // srand((unsigned)time(NULL));
-        
-        // cellData[cellIndex][0]=((double)rand() / (RAND_MAX+1)) ;
-        // cellData[cellIndex][1]=((double)rand() / (RAND_MAX+1)) ;
-        // double tmp=-std::sqrt(cellData[cellIndex][0]*cellData[cellIndex][0]+cellData[cellIndex][1]*cellData[cellIndex][1]); 
-        
-        // cellData[cellIndex][0]/=tmp;
-        // cellData[cellIndex][1]/=tmp;
-        // std::cerr<< "MT vector"<< cellData[cellIndex][0]<<" "<< cellData[cellIndex][1]<< std::endl;
+          // Set center position to com of the cell
+          for (size_t d=0; d<dimension; ++d)
+            cellData[cellIndex][numVariable+d] = com[d];    
+          // Set internal wall lengths to the distance btw com and the vertex
+          for (size_t k=0; k<numInternalWall; ++k) {
+            cellData[cellIndex][numVariable+dimension+k] = T.cell(cellIndex).edgeLength(k);
+          }
+         
+        }
       }
     }
-    else { 
-      // Copy central position and edge length data from cells in Tissue
+    if (numParameter()==2 && parameter(1)==1){ // double resting length
+      if (!T.cell(0).numCenterPosition() || parameter(0)==1 ) {
+        //No centers defined in cells in tissue or overridden (create central triangulation from scratch)
+        for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {	
+          size_t numInternalWall = T.cell(cellIndex).numVertex();
+          cellData[cellIndex].resize(numVariable+dimension+3*numInternalWall);
+          cellDerivs[cellIndex].resize(numVariable+dimension+3*numInternalWall);
+          com = T.cell(cellIndex).positionFromVertex(vertexData);
+          // Set center position to com of the cell
+          for (size_t d=0; d<dimension; ++d)
+            cellData[cellIndex][numVariable+d] = com[d];    
+          // Set internal wall lengths to the distance btw com and the vertex
+          for (size_t k=0; k<numInternalWall; ++k) {// internal walls
+            Vertex *tmpVertex = T.cell(cellIndex).vertex(k); 
+            size_t vertexIndex = tmpVertex->index();
+            double distance = std::sqrt( (com[0]-vertexData[vertexIndex][0])*
+                                         (com[0]-vertexData[vertexIndex][0])+
+                                         (com[1]-vertexData[vertexIndex][1])*
+                                         (com[1]-vertexData[vertexIndex][1])+
+                                         (com[2]-vertexData[vertexIndex][2])*
+                                         (com[2]-vertexData[vertexIndex][2]) );   
+            cellData[cellIndex][numVariable+dimension+2*k] = distance;
+            cellData[cellIndex][numVariable+dimension+2*k+1] = distance;
+          }
+          for (size_t k=0; k<numInternalWall; ++k) { // growing component of main walls zero at initiation
+            size_t numInternalWall = T.cell(cellIndex).numVertex();
+            cellData[cellIndex][numVariable+dimension+2*numInternalWall+k] =0;
+          }
+          // sleep(1); //initiating MT dirrections randomly in xy plane
+          // srand((unsigned)time(NULL));
+        
+          // cellData[cellIndex][0]=((double)rand() / (RAND_MAX+1)) ;
+          // cellData[cellIndex][1]=((double)rand() / (RAND_MAX+1)) ;
+          // double tmp=-std::sqrt(cellData[cellIndex][0]*cellData[cellIndex][0]
+          //                      +cellData[cellIndex][1]*cellData[cellIndex][1]); 
+          
+          // cellData[cellIndex][0]/=tmp;
+          // cellData[cellIndex][1]/=tmp;
+          // std::cerr<< "MT vector"<< cellData[cellIndex][0]<<" "<< cellData[cellIndex][1]<< std::endl;
+        }
+      }
+      // else { // double resting length (not working yet) !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      //   // Copy central position and edge length data from cells in Tissue
+      //   for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {
+      //     size_t numInternalWall = T.cell(cellIndex).numVertex();
+      //     cellData[cellIndex].resize(numVariable+dimension+3*numInternalWall);
+      //     cellDerivs[cellIndex].resize(numVariable+dimension+3*numInternalWall);
+      //     com = T.cell(cellIndex).centerPosition();
+      //     // Set center position to com of the cell
+      //     for (size_t d=0; d<dimension; ++d)
+      //       cellData[cellIndex][numVariable+d] = com[d];    
+      //     // Set internal wall lengths to the distance btw com and the vertex
+      //     for (size_t k=0; k<2*numInternalWall; ++k) {                              // should be modified <<<<<<<<<<
+      //       cellData[cellIndex][numVariable+dimension+k] = T.cell(cellIndex).edgeLength(k);
+      //     }
+      //   }
+      // }
+    }
+    if (numParameter()==2 && parameter(1)==2){ // double resting length as continuation of  single resting length
       for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {
-	size_t numInternalWall = T.cell(cellIndex).numVertex();
-	cellData[cellIndex].resize(numVariable+dimension+numInternalWall);
-	cellDerivs[cellIndex].resize(numVariable+dimension+numInternalWall);
-	com = T.cell(cellIndex).centerPosition();
-	// Set center position to com of the cell
-	for (size_t d=0; d<dimension; ++d)
-	  cellData[cellIndex][numVariable+d] = com[d];    
-	// Set internal wall lengths to the distance btw com and the vertex
-	for (size_t k=0; k<numInternalWall; ++k) {
-	  cellData[cellIndex][numVariable+dimension+k] = T.cell(cellIndex).edgeLength(k);
-	}
+        
+        size_t numInternalWall = T.cell(cellIndex).numVertex();
+        com = T.cell(cellIndex).centerPosition();
+        
+        cellData[cellIndex].resize(numVariable+dimension+3*numInternalWall);
+        cellDerivs[cellIndex].resize(numVariable+dimension+3*numInternalWall);
+        
+        for (size_t k=0; k<dimension; ++k) {// center position
+          cellData[cellIndex][numVariable+k] = com[k];
+        }
+   
+        for (size_t k=0; k<numInternalWall; ++k) {// internal walls
+          cellData[cellIndex][numVariable+dimension+2*k] = T.cell(cellIndex).edgeLength(k);
+          cellData[cellIndex][numVariable+dimension+2*k+1] =T.cell(cellIndex).edgeLength(k); 
+        }
+        
+        for (size_t k=0; k<numInternalWall; ++k) { // growing component of main walls zero at initiation
+          cellData[cellIndex][numVariable+dimension+2*numInternalWall+k] =0;
+        }
       }
     }
   }
