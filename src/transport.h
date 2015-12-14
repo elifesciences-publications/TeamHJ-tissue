4bf8fbd2b5da906e3b9842b144ffce1ca2bab515
Behruz Bozorg <behruz@thep.lu.se>
2015-12-14 16:44:40 +0000

    Diffusion2d added

--- src/transport.h
+++ src/transport.h
@@ -138,6 +138,50 @@ class DiffusionSimple : public BaseReaction {
 
 
 
+///
+/// @brief A cell-to-cell diffusion reaction
+///
+/// A reaction for passive diffusion of molecules between neighboring cells.
+/// Unlike DiffusionSimple, cell volume and other topological properties are taken into account.
+/// The diffusion is described by the equation
+///  
+/// @f[ \frac{dc_{i}}{dt} = - p_0 A^{-1} \sum_j  L_{ij} ( c_{i} - c_{j}) @f] 
+///  
+/// where p_0 is the diffusion rate,$L_{ij}$ is the contact length between cells, 
+/// $A^{-1}$ is the cell volume, $c_i$ is the cell concentration and $c_j$ is the 
+/// concentration in a neighboring cell.
+///  
+/// In a model file the reaction is defined as
+///
+/// @verbatim
+/// Diffusion2d 1 1 1
+/// p_0
+/// c_index
+/// @endverbatim
+///
+///
+/// @note The Simple in the name reflects the fact that no geometric factors are included.
+///
+class Diffusion2d : public BaseReaction {
+  
+public:
+  
+  Diffusion2d(std::vector<double> &paraValue, 
+              std::vector< std::vector<size_t> > 
+              &indValue );
+  
+  void derivs(Tissue &T,
+	      DataMatrix &cellData,
+	      DataMatrix &wallData,
+	      DataMatrix &vertexData,
+	      DataMatrix &cellDerivs,
+	      DataMatrix &wallDerivs,
+	      DataMatrix &vertexDerivs );
+};
+
+
+
+
 
 
 ///
